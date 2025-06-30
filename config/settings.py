ec6454a41aa4ec8e9032fae7f6bb2e08e72010c8
GABRIEL CESAR PEREIRA <gabriel.pereira@rn.local>
2025-06-30 15:23:56 -0300

    add .env

--- config/settings.py
+++ config/settings.py
@@ -4,12 +4,13 @@ Django settings for ScoutPro project.
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rtu_g$x-o^ey44h(lv*!y*9r+%fim_4do74#0h5&$$=4mg*r*u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

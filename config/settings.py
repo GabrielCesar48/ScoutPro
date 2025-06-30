f7d38ed7ec87a337ce3560508dd2d1919b59f041
GABRIEL CESAR PEREIRA <gabriel.pereira@rn.local>
2025-06-30 17:17:41 -0300

    Alterando requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -4,18 +4,15 @@ Django settings for ScoutPro project.
 
 from pathlib import Path
 import os
-from decouple import config
+from decouple import config, Csv
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
